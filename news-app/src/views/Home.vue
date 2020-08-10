5181bf543ce9d681bde771244d21f88c656ef1f8
Skipups <svetlana.rovinsky@gmail.com>
2020-08-10 16:59:38 -0400

    homepage dates in procress

--- news-app/src/views/Home.vue
+++ news-app/src/views/Home.vue
@@ -15,7 +15,18 @@
           v-model="filterQuery"
           @input="loadFilter"
         ></v-text-field>
+        <v-text-field
       </v-flex>
+      <div>
+        <h3>Search by Date</h3>
+        <div>
+          <h4 class="textInDateSearch">From:</h4>
+          <v-text-field v-model="fromDate"></v-text-field>
+          <h4 class="textInDateSearch">To:</h4>
+          <v-text-field v-model="toDate"></v-text-field>
+          <button v-on:click="this.filterByDate">Submit</button>
+        </div>
+      </div>
       <v-flex
         xs12
         sm12
@@ -46,15 +57,33 @@ export default {
   data: () => ({
     articles: [],
     filterQuery: '',
-    contentType: 'top UK headlines'
+    contentType: 'top UK headlines',
+    fromDate: '2020-07-01',
+    toDate: '2020-07-01'
   }),
   created() {
     console.log(process.env[`VUE_APP_SERVICE_URL_${stage}`])
     this.loadArticles('headlines', JSON.stringify({ country: 'gb' }))
   },
+
   methods: {
+    filterByDate: debounce(function loadFilter(input, fromDate, toDate) {
+      console.log(input, fromDate, toDate)
+      input === '' ? 'gb' : this.filterQuery
+      if (fromDate && toDate) {
+        this.contentType = `search results for: ${input}, from: ${fromDate} to: ${toDate}`
+        this.loadArticles(
+          'search',
+          JSON.stringify({ from: fromDate, to: toDate, q: input })
+        )
+        this.fromDate = null
+        this.toDate = null
+      }
+    }, 500),
+
     loadFilter: debounce(function loadFilter(input) {
       if (input) {
+        console.log('filterQuery', this.filterQuery)
         this.contentType = `search results for: ${input}`
         this.loadArticles('search', JSON.stringify({ q: input }))
       } else {
@@ -63,6 +92,7 @@ export default {
       }
     }, 500),
     loadArticles(type, params) {
+      console.log('type', type, 'params', params)
       axios
         .post(`/articles?type=${type}`, params)
         .then(response => {
